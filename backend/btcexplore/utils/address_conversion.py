4720551191c3bba4ef34df2714e77157acc30ed2
Kevin Audleman <kevin.audleman@gmail.com>
2021-10-04 23:34:35 -0700

    cleanup

--- backend/btcexplore/utils/address_conversion.py
+++ backend/btcexplore/utils/address_conversion.py
@@ -13,25 +13,18 @@ def address_from_public_key(pub_key):
     """
 
     # 1 - SHA-256 hash of pub_key
-    sha = hashlib.sha256()
-    sha.update(bytearray.fromhex(pub_key))
+    sha = hashlib.sha256(bytearray.fromhex(pub_key))
 
     # 2 - RIPEMD-160 of 1
-    rip = hashlib.new('ripemd160')
-    rip.update(sha.digest())
-    ripe_str = rip.hexdigest()
+    ripe = hashlib.new('ripemd160', sha.digest()).hexdigest()
 
     # 3 - Add network bytes to 2
-    key_hash = f'00{ripe_str}'
+    key_hash = f'00{ripe}'
 
     # 4 - Double SHA-256 of 3 to generate checksum
-    sha = hashlib.sha256()
-    sha.update(bytearray.fromhex(key_hash))
-    checksum = sha.digest()
-    sha = hashlib.sha256()
-    sha.update(checksum)
-    checksum = sha.hexdigest()[0:8]
-    
+    checksum = hashlib.sha256(bytearray.fromhex(key_hash)).digest()
+    checksum = hashlib.sha256(checksum).hexdigest()[0:8]
+
     # 5 - Add 4 to end of 3
     key_hash = bytes(bytearray.fromhex(key_hash + checksum))
 
