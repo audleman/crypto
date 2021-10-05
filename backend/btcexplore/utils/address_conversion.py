7b22696447d173b8b09f70db3dcc77382720de72
Kevin Audleman <kevin.audleman@gmail.com>
2021-10-04 23:44:39 -0700

    wip

--- backend/btcexplore/utils/address_conversion.py
+++ backend/btcexplore/utils/address_conversion.py
@@ -1,5 +1,11 @@
-#!/usr/bin/env python
-# https://en.bitcoin.it/wiki/Protocol_documentation#Addresses
+"""
+https://en.bitcoin.it/wiki/Protocol_documentation#Addresses
+
+Version = 1 byte of 0 (zero); on the test network, this is 1 byte of 111
+Key hash = Version concatenated with RIPEMD-160(SHA-256(public key))
+Checksum = 1st 4 bytes of SHA-256(SHA-256(Key hash))
+Bitcoin Address = Base58Encode(Key hash concatenated with Checksum)
+"""
 
 import hashlib
 import base58
@@ -26,9 +32,9 @@ def address_from_public_key(pub_key):
     checksum = hashlib.sha256(checksum).hexdigest()[0:8]
 
     # 5 - Add 4 to end of 3
-    key_hash = bytes(bytearray.fromhex(key_hash + checksum))
+    key_hash_plus_checksum = bytes(bytearray.fromhex(key_hash + checksum))
 
     # 6 - Base58 encoding of 5
-    address = base58.b58encode(key_hash).decode('utf-8')
+    address = base58.b58encode(key_hash_plus_checksum).decode('utf-8')
 
     return address
